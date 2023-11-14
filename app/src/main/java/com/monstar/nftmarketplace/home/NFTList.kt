29805a3b22ec15daaf67213e5c4ce4d4b0bd361b
Solomon Evoh <solomon.evoh@gmail.com>
2023-11-14 10:34:21 +0100

    Created the NFT List and added the NFT card to it

--- app/src/main/java/com/monstar/nftmarketplace/home/NFTList.kt
+++ app/src/main/java/com/monstar/nftmarketplace/home/NFTList.kt
@@ -1,2 +1,39 @@
 package com.monstar.nftmarketplace.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.monstar.nftmarketplace.nfts
+import com.monstar.nftmarketplace.ui.theme.NFTMarketplaceTheme
+
+@Composable
+fun NFTList() {
+    LazyRow(
+        modifier = Modifier.padding(bottom = 30.dp, top = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        items(nfts) { nft ->
+            NFTCard(
+                title = nft.title,
+                subTitle = nft.subTitle,
+                image = painterResource(id = nft.image),
+                likes = nft.likes,
+                eth = nft.eth
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewNFTList() {
+    NFTMarketplaceTheme {
+        NFTList()
+    }
+}
