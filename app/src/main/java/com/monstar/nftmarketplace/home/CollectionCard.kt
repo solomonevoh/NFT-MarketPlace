7c9d1aab2916790183fc7c42739a0e47bf624533
Solomon Evoh <solomon.evoh@gmail.com>
2023-11-14 08:15:30 +0100

    Created the Collection Card composable component

--- app/src/main/java/com/monstar/nftmarketplace/home/CollectionCard.kt
+++ app/src/main/java/com/monstar/nftmarketplace/home/CollectionCard.kt
@@ -1,15 +1,109 @@
 package com.monstar.nftmarketplace.home
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.monstar.nftmarketplace.collections
 import com.monstar.nftmarketplace.ui.theme.NFTMarketplaceTheme
 
 @Composable
 fun CollectionCard(title: String, image: Painter, likes: Int) {
-Column
+    var isLiked by remember {
+        mutableStateOf(false)
+    }
+    Column(
+        modifier = Modifier
+            .width(216.dp)
+            .height(216.dp)
+            .border(BorderStroke(1.dp, Color.White.copy(0.5f)), shape = RoundedCornerShape(30.dp))
+
+    ) {
+        Image(
+            image,
+            contentDescription = "NFT Image",
+            modifier = Modifier
+                .width(155.dp)
+                .height(155.dp)
+                .padding(10.dp)
+                .border(
+                    BorderStroke(1.dp, Color.White.copy(0.5f)),
+                    shape = RoundedCornerShape(22.dp)
+                )
+                .fillMaxSize()
+                .clip(RoundedCornerShape(22.dp))
+        )
+        Row(modifier = Modifier.padding(10.dp), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                title,
+                fontSize = 13.sp,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Left,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconToggleButton(checked = isLiked, onCheckedChange = {
+                    isLiked = !isLiked
+                }, modifier = Modifier.size(13.dp)) {
+                    Icon(
+                        tint = if (isLiked) {
+                            Color.Red
+                        } else {
+                            Color(235, 235, 245).copy(0.6f)
+                        },
+                        imageVector = if (isLiked) {
+                            Icons.Filled.Favorite
+                        } else {
+                            Icons.Default.FavoriteBorder
+                        },
+                        contentDescription = "Favourite Button"
+                    )
+                }
+                Text(
+                    likes.toString(),
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 13.sp,
+                    textAlign = TextAlign.Right,
+                    color = Color(235, 235, 245).copy(0.6f)
+                )
+            }
+        }
+    }
 }
 
 @Preview
