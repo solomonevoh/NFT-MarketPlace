9a4d6d8215b51cb8fcb7c4f4db6693b885ca9278
Solomon Evoh <solomon.evoh@gmail.com>
2023-11-16 22:17:32 +0100

    Fixed the top app bar in the Home screen

--- app/src/main/java/com/monstar/nftmarketplace/home/HomeScreen.kt
+++ app/src/main/java/com/monstar/nftmarketplace/home/HomeScreen.kt
@@ -8,10 +8,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -29,7 +29,7 @@ import com.monstar.nftmarketplace.ui.theme.NFTMarketplaceTheme
 fun HomeScreen() {
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
+            TopAppBar(
                 title = {
                     Text(
                         "NFT Marketplace",
